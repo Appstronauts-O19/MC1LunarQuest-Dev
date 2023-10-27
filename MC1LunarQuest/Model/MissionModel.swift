5cbff7b36eda562286e09239ed99b077f789680a
Arantza Castro <ary.castro.dessavre@gmail.com>
2023-10-27 17:28:04 +0200

    Mission MOdel as well as the Missions Page view (elimanated mission card view)

--- MC1LunarQuest/Model/MissionModel.swift
+++ MC1LunarQuest/Model/MissionModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct MissionModel: Identifiable{
+    var id: UUID = UUID()
+    var missionImage: String
+    var missionName: String
+    var missionDescription: String
+    var badgeName: String
+    var completed: Bool = false
+}
+
+class ViewMissionModel {
+    let missions: [MissionModel] = [
+        MissionModel(missionImage: "Mission1", missionName: "Moon Exploration", missionDescription: "Explore the moon surface and investigate your findings", badgeName: "Badge1"),
+        MissionModel(missionImage: "Mission2", missionName: "Mars Exploration", missionDescription: "Is Mars hospitable? Find out what would humankind need to live in Mars.", badgeName: ""),
+        MissionModel(missionImage: "Mission3", missionName: "To infinity and beyond", missionDescription: "What else is out there? ...", badgeName: "")
+    ]
+}
--- MC1LunarQuest/Views/MissionsPageView.swift
+++ MC1LunarQuest/Views/MissionsPageView.swift
@@ -8,16 +8,107 @@
 import SwiftUI
 
 struct MissionsPageView: View {
+    
+    var viewMissionModel = ViewMissionModel()
+    
     var body: some View {
-        NavigationStack{
-            VStack {
-                MissionCardView()
+        NavigationStack {
+            Text("Missions")
+                .fontWeight(.heavy)
+                .foregroundStyle(Color.white)
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            
+            ScrollView {
+                ForEach (viewMissionModel.missions, id: \.id){exm in
+                    ZStack (alignment: .bottomLeading){
+                        // Background Image
+                        Image(exm.missionImage)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .shadow(radius: 10)
+                            .overlay(alignment: .bottom , content:{
+                                HStack{
+                                    VStack(alignment: .leading) {
+                                        Text(exm.missionName)
+                                            .font(.headline)
+                                            .foregroundColor(.white)
+                                        Text(exm.missionDescription)
+                                            .font(.subheadline)
+                                            .foregroundColor(.white)
+                                    }
+                                    .padding()
+                                    
+                                    Spacer()
+                                    
+                                    NavigationLink(destination: MissionView()){
+                                        Image(systemName: "play.fill")
+                                        Text("Play")
+                                    }
+                                    .foregroundStyle(.purple)
+                                    .tint(.white)
+                                    .buttonStyle(.borderedProminent)
+                                    .buttonBorderShape(.capsule)
+                                    .padding()
+                                    
+                                }
+                                .background(.ultraThinMaterial)
+                                }
+                            )
+                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                    }
+                    .padding()
+                }
             }
-            //.background(RadialGradient(gradient: Gradient(colors: [Color.purple, Color.black]), center: .init(x: 0.5, y: 1.3), startRadius: 10, endRadius: 800))
+            
+        .background(RadialGradient(gradient: Gradient(colors: [Color.purple, Color.black]), center: .init(x: 0.5, y: 1.3), startRadius: 10, endRadius: 800))
         }
     }
 }
 
+
+
+struct RoundedCorners: Shape {
+    var tl: CGFloat = 0.0
+    var tr: CGFloat = 0.0
+    var bl: CGFloat = 0.0
+    var br: CGFloat = 0.0
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        let width = rect.size.width
+        let height = rect.size.height
+
+        // Make sure the radius does not exceed the bounds dimensions
+        let tr = min(min(self.tr, height/2), width/2)
+        let tl = min(min(self.tl, height/2), width/2)
+        let bl = min(min(self.bl, height/2), width/2)
+        let br = min(min(self.br, height/2), width/2)
+        
+        path.move(to: CGPoint(x: width / 2.0, y: 0))
+        path.addLine(to: CGPoint(x: width - tr, y: 0))
+        path.addArc(center: CGPoint(x: width - tr, y: tr), radius: tr,
+                    startAngle: Angle(degrees: -90), endAngle: Angle(degrees: 0), clockwise: false)
+        path.addLine(to: CGPoint(x: width, y: height - br))
+        path.addArc(center: CGPoint(x: width - br, y: height - br), radius: br,
+                    startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 90), clockwise: false)
+        path.addLine(to: CGPoint(x: bl, y: height))
+        path.addArc(center: CGPoint(x: bl, y: height - bl), radius: bl,
+                    startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 180), clockwise: false)
+        path.addLine(to: CGPoint(x: 0, y: tl))
+        path.addArc(center: CGPoint(x: tl, y: tl), radius: tl,
+                    startAngle: Angle(degrees: 180), endAngle: Angle(degrees: 270), clockwise: false)
+        path.closeSubpath()
+
+        return path
+    }
+}
+
+
 #Preview {
     MissionsPageView()
 }
+
+
+
+    
